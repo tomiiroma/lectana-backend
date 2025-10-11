35dda9fb5b4e02a4998bccb7781d6764ae25e4a1
Tomas Roma <tomas.roma@davinci.edu.ar>
2025-10-11 18:13:10 -0300

    a

--- src/server.js
+++ src/server.js
@@ -2,6 +2,6 @@ import app from './app.js';
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Lectana API running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Lectana API running on port ${PORT}`);
 });
